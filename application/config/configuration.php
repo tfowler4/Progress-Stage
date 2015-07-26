b6b627294995fc52edad0e7b3ada31f436e48d87
Terrijon Fowler <terrijonfowler@gmail.com>
2015-07-25 21:18:05 -0400

    Make configuration files be adaptable to local/webserver

--- application/config/configuration.php
+++ application/config/configuration.php
@@ -2,36 +2,39 @@
 date_default_timezone_set('America/Los_Angeles');
 
 // Global Settings
-define('GAME_NAME_1',       'Rift');
-define('HOST_NAME',         'http://localhost/stage');
-define('DOMAIN',            'stage');
+// http://www.topofrift.com
+// http://www.wildstar-progress.com
+// http://stage.trinityguild.org
+// http://localhost/stage
+
+define('GAME_NAME_1', 'Rift');
+define('LIVE',        0);
+
+if ( strpos($_SERVER['DOCUMENT_ROOT'], '/xampp/htdocs') !== FALSE ) { // Local Machine
+    if ( GAME_NAME_1 == 'Rift' && LIVE == 1 ) { define('HOST_NAME', 'http://localhost/site-rift'); define('DOMAIN', 'site-rift'); }
+    if ( GAME_NAME_1 == 'WildStar' && LIVE == 1 ) { define('HOST_NAME', 'http://localhost/site-wildstar'); define('DOMAIN', 'site-wildstar'); }
+    if ( !empty(GAME_NAME_1) && LIVE == 0 ) { define('HOST_NAME', 'http://localhost/stage'); define('DOMAIN', 'stage'); }
+} elseif ( strpos($_SERVER['DOCUMENT_ROOT'], '/public_html/') !== FALSE ) { // Webserver
+    if ( GAME_NAME_1 == 'Rift' && LIVE == 1 ) { define('HOST_NAME', 'http://www.topofrift.com'); }
+    if ( GAME_NAME_1 == 'WildStar' && LIVE == 1 ) { define('HOST_NAME', 'http://www.wildstar-progress.com'); }
+    if ( !empty(GAME_NAME_1) && LIVE == 0 ) { define('HOST_NAME', 'http://stage.trinityguild.org'); }
+}
+
+if ( empty(DOMAIN) ) {
+    define('DOMAIN', '');
+}
+
 define('DEFAULT_TIME_ZONE', 'America/Los_Angeles');
-define('LIVE',              0);
 define('SITE_ONLINE',       1);
 define('AD_HEADER',         0);
 define('AD_SIDE',           0);
-
 define('PASSWORD_MINIMUM',  3);
 define('MAX_IMAGE_SIZE',    400000000); // 400 MB
+define('POINT_BASE',       1000); // QP, AP
+define('POINT_FINAL_BASE', 5000); // AP
+define('POINT_BASE_MOD',   2500); // APF
 
-define('POINT_BASE',       1000);
-define('POINT_FINAL_BASE', 5000);
-define('POINT_BASE_MOD',   2500);
-
-define('BITLY_TOKEN', '14b2b2e1c7525700db1573084e1a64ee45fb33da');
-
-// APF
-define('POINT_FINAL_BASE_NEW', serialize(array('10' => 5000,'20' => 25000)));
-
-// APF
-define('POINT_BASE_MOD_NEW', serialize(array('10' => 2500,'20' => 12500)));
-
-// QP
-define('POINT_BASE_NEW', serialize(array('10' => 1000, '20' => 5000)));
-
-define('RANK_SYSTEMS',   serialize(array('QP' => 'Quality Progression', 'AP' => 'Aeyths Point', 'APF' => 'Aeyths Point Flat',)));
-
-//Temporary storage of abbreviations as it will be shifted to the RankSystem Class
+define('RANK_SYSTEMS',   serialize(array('QP' => 'Quality Progression', 'AP' => 'Aeyths Point', 'APF' => 'Aeyths Point Flat')));
 
 define('VALID_IMAGE_FORMATS', serialize(array(IMAGETYPE_GIF, IMAGETYPE_JPEG, IMAGETYPE_PNG, IMAGETYPE_BMP)));
 
@@ -49,19 +52,20 @@ include ABSOLUTE_PATH . '/library/facebook/src/facebook.php';
 include ABSOLUTE_PATH . '/library/twitter/codebird-php-master/src/codebird.php';
 
 // Bitly
+define('BITLY_TOKEN', '14b2b2e1c7525700db1573084e1a64ee45fb33da');
 include ABSOLUTE_PATH . '/library/BitlyPHP/bitly.php';
+
+// PHPMailer
+include ABSOLUTE_PATH . '/library/PHPMailer/PHPMailerAutoload.php';
 
 foreach ( glob($_SERVER['DOCUMENT_ROOT'] . '/' . DOMAIN . '/application/lib/*.php') as $fileName ) { include $fileName; }
 foreach ( glob($_SERVER['DOCUMENT_ROOT'] . '/' . DOMAIN . '/application/utils/*.php') as $fileName ) { include $fileName; }
 foreach ( glob($_SERVER['DOCUMENT_ROOT'] . '/' . DOMAIN . '/application/services/*.php') as $fileName ) { include $fileName; }
 
-include dirname(dirname(dirname(__FILE__))) . '/library/PHPMailer/PHPMailerAutoload.php';
-
-$sid = session_id();
-
+// Begin Compression
 ob_start('ob_gzhandler');
 
-if ( !isset($sid) || $sid == "" ) {
+// Begin Session
+if ( session_id() == '' || !isset($_SESSION) ) {
     session_start();
-    $sid = session_id();
 }
