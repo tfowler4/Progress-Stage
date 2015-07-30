027774558af8a1f6cc1caa11ad84aeb98550b17c
Vannary Pov <vannary.pov@gmail.com>
2015-07-29 22:17:49 -0400

    Adding importCurrentBackupFileToDB Script

--- scripts/importCurrentBackupFileToDB.php
+++ scripts/importCurrentBackupFileToDB.php
@@ -5,8 +5,8 @@ class importCurrentBackupFileToDB {
 	static protected $_login;
 	static protected $_connection;
     static protected $_serverBackupPath;
-    //C:/xampp/htdocs/stage/tmp
-    const LOCAL_PATH = ABSOLUTE_PATH . '/tmp';
+
+    const LOCAL_PATH = ABSOLUTE_PATH . '/temp';
 
 	static public function init() {
 		$dateYear  = date('Y');
@@ -14,8 +14,7 @@ class importCurrentBackupFileToDB {
         $dateDay   = date('d');
         $fullDate  = $dateYear . '/' . $dateMonth . '/' . $dateDay;
 
-        //public_html/site-rift/data/backups/2015/7-Jul/26
-        self::$_serverBackupPath = 'public_html/site-rift/data/backups/'. $fullDate;
+        self::$_serverBackupPath = 'public_html/' . DOMAIN . '/data/backups/'. $fullDate;
         self::$_connection       = ftp_connect(FTP_HOST);
         self::$_login            = ftp_login(self::$_connection, FTP_USER, FTP_PASSWORD);
 
@@ -23,33 +22,58 @@ class importCurrentBackupFileToDB {
         if ( (!self::$_connection) || (!self::$_login) ) {
             die;
         }
-        //public_html/site-rift/data/backups/2015/7-Jul/27/vgtrin5_rift_live2015-07-27_10-00.sql
+
+        // To set the backup file
         $lastestBackupFile = self::getCurrentBackupFile(self::$_connection, self::$_serverBackupPath);
-        //print_r($lastestBackupFile);
-        $importFile = self::downloadBackupFile(self::$_connection, $lastestBackupFile));
+
+        // To set the import file
+        $importFile = self::downloadBackupFile(self::$_connection, $lastestBackupFile);
+
+        self::executeImportCommand($importFile);
+
         ftp_close(self::$_connection);
 	}
 
     // To import file into database
     static public function executeImportCommand($file) {
-        $command='mysql -h' . DB_HOST .' -u' . DB_USER .' -p' . DB_PASS .' ' . DB_NAME .' < ' . $file;
-        exec($command);
+
+        // To import file into target database; DB must already exist
+        $command ='C:\xampp\mysql\bin\mysql -h' . DB_HOST .' -u' . DB_USER .' -p' . DB_PASS .' ' .  DB_NAME .' < ' . $file; //. ' > stdout_output.txt 2>stderr_output.txt';
+
+        exec($command, $output=array(), $msg);
+
+        switch($msg) {
+            case 0:
+                echo 'SUCCESS: Import Completed!';
+                break;
+            case 1:
+                echo 'ERROR: Import Failed!';
+                break;
+        }
     }
 
     // To download lastest file to local
     static public function downloadBackupFile($connection, $file) {
+        if ( !file_exists(self::LOCAL_PATH) ) {
+            mkdir(self::LOCAL_PATH, 0777, true);
+        }
+
         $localFile = str_replace(self::$_serverBackupPath, self::LOCAL_PATH, $file);
 
         if ( !ftp_get($connection, $localFile, $file, FTP_BINARY) ) {
             echo 'Error Downloading: ' . $file . '<br>';
             exit;
         }
-
         return $localFile;
     }
 
 	// To get lastest backup file from web server
 	static public function getCurrentBackupFile($connection, $path) {
+        if (DOMAIN == 'stage') {
+            echo 'ERROR: Please execute script from the correct domain!';
+            exit;
+        }
+
 		$listings   = ftp_nlist($connection, $path);
         $count      = count($listings);
         $lastestFile = $listings[$count - 1];
@@ -60,4 +84,3 @@ class importCurrentBackupFileToDB {
 
 importCurrentBackupFileToDB::init();
 
-//$command='mysql -h' .$mysqlHostName .' -u' .$mysqlUserName .' -p' .$mysqlPassword .' ' .$mysqlDatabaseName .' < ' .$mysqlImportFilename;
