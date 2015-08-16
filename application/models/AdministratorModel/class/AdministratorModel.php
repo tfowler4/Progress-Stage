45f3352f1247affb6d4428f0f2b67f270a23cc9c
Vannary Pov <vannary.pov@gmail.com>
2015-08-16 01:03:19 -0400

    Adding Edit Functionality to Admin Model

--- application/models/AdministratorModel/class/AdministratorModel.php
+++ application/models/AdministratorModel/class/AdministratorModel.php
@@ -1,117 +1,183 @@
 <?php 
 
 class AdministratorModel extends Model {
-	protected $_userDetails;
+    protected $_userDetails;
 
-	public function __construct($module, $params) {
-		parent::__construct($module);
+    public function __construct($module, $params) {
+        parent::__construct($module);
 
-		if (isset($_SESSION['userDetails']) ) {
-			$this->_userDetails = $_SESSION['userDetails'];
-			
-			if ($this->_userDetails->_admin != 1) {
-				header('Location: ' . HOST_NAME);	
-			}
-		} else {
-			header('Location: ' . HOST_NAME);
-		}
+        if (isset($_SESSION['userDetails']) ) {
+            $this->_userDetails = $_SESSION['userDetails'];
+            
+            if ($this->_userDetails->_admin != 1) {
+                header('Location: ' . HOST_NAME);
+            }
+        } else {
+            header('Location: ' . HOST_NAME);
+        }
 
-		if (isset($_POST['request']) ) {
+        if (isset($_POST['request']) ) {
 
-			switch ($_POST['request']) {
-				case "tier-add":
-					$this->addNewTier();
-					break;
-				case "dungeon-add":
-					$this->addNewDungeon();
-					break;
-				case "encounter-add":
-					$this->addNewEncounter();
-					break;
-				case "guild-add":
-					$this->addNewGuild();
-					break;
-			}
-		}
-		
-	}
+            switch ($_POST['request']) {
+                case "tier-add":
+                    $this->addNewTier();
+                    break;
+                case "dungeon-add":
+                    $this->addNewDungeon();
+                    break;
+                case "encounter-add":
+                    $this->addNewEncounter();
+                    break;
+                case "guild-add":
+                    $this->addNewGuild();
+                    break;
+                case "guild-remove":
+                    $this->removeGuild();
+                    break;
+                case "guild-edit":
+                    $this->editGuild();
+                    break;
+                //case "guild-edit-details":
+                    //$this->editGuildDetails();
+                    //break;
+            }
+        }
+    }
 
-	public function addNewTier() {
+    public function addNewTier() {
+        $tier      = $_POST['form'][0]['value'];
+        $altName   = $_POST['form'][1]['value'];
+        $tierName  = $_POST['form'][2]['value'];
+        $startDate = $_POST['form'][5]['value'] . '-' . $_POST['form'][3]['value'] . '-' .$_POST['form'][4]['value'];
 
-		$tier 		= $_POST['form'][0]['value'];
-		$altName 	= $_POST['form'][1]['value'];
-		$tierName 	= $_POST['form'][2]['value'];
-		$startDate	= $_POST['form'][5]['value'] . '-' . $_POST['form'][3]['value'] . '-' .$_POST['form'][4]['value'];
+        $sqlString = sprintf(
+            "INSERT INTO %s
+            (tier, alt_tier, title, date_start)
+            values('%s', '%s', '%s', '%s')",
+            DbFactory::TABLE_TIERS,
+            $tier,
+            $altName,
+            $tierName,
+            $startDate
+            );
+        die;
+    }
 
-		$sqlString = sprintf(
-			"INSERT INTO %s
-			(tier, alt_tier, title, date_start)
-			values('%s', '%s', '%s', '%s')",
-			DbFactory::TABLE_TIERS,
-			$tier,
-			$altName,
-			$tierName,
-			$startDate
-			);
+    public function addNewDungeon() {
+        $dungeon   = $_POST['form'][0]['value'];
+        $tier      = $_POST['form'][1]['value'];
+        $numOfMobs = $_POST['form'][2]['value'];
 
-		die;
-	}
+        $sqlString = sprintf(
+            "INSERT INTO %s
+            (name, tier, mobs)
+            values('%s', '%s', '%s')",
+            DbFactory::TABLE_DUNGEONS,
+            $dungeon,
+            $tier,
+            $numOfMobs
+            );
+        die;
+    }
 
-	public function addNewDungeon() {
-		$dungeon 	= $_POST['form'][0]['value'];
-		$tier 		= $_POST['form'][1]['value'];
-		$numOfMobs 	= $_POST['form'][2]['value'];
+    public function addNewEncounter() {
+        $encounter = $_POST['form'][0]['value'];
+        $dungeon   = $_POST['form'][1]['value'];
+        $tier      = $_POST['form'][2]['value'];
+        $raidSize  = $_POST['form'][3]['value'];
 
-		$sqlString = sprintf(
-			"INSERT INTO %s
-			(name, tier, mobs)
-			values('%s', '%s', '%s')",
-			DbFactory::TABLE_DUNGEONS,
-			$dungeon,
-			$tier,
-			$numOfMobs
-			);
+        $sqlString = sprintf(
+            "INSERT INTO %s
+            (name, dungeon, tier, players)
+            values('%s', '%s', '%s', '%s')",
+            DbFactory::TABLE_ENCOUNTERS,
+            $encounter,
+            $dungeon,
+            $tier,
+            $raidSize
+            );
+        die;
+    }
 
-		die;		
-	}
+    public function addNewGuild() {
+        $guild   = $_POST['form'][0]['value'];
+        $server  = $_POST['form'][1]['value'];
+        $country = $_POST['form'][2]['value'];
 
-	public function addNewEncounter() {
-		$encounter 		= $_POST['form'][0]['value'];
-		$dungeon 		= $_POST['form'][1]['value'];
-		$tier 			= $_POST['form'][2]['value'];
-		$raidSize 		= $_POST['form'][3]['value'];
+        $sqlString = sprintf(
+            "INSERT INTO %s
+            (name, server, country)
+            values('%s', '%s', '%s')",
+            DbFactory::TABLE_GUILDS,
+            $guild,
+            $server,
+            $country
+            );
+        die;
+    }
 
-		$sqlString = sprintf(
-			"INSERT INTO %s
-			(name, dungeon, tier, players)
-			values('%s', '%s', '%s', '%s')",
-			DbFactory::TABLE_ENCOUNTERS,
-			$encounter,
-			$dungeon,
-			$tier,
-			$raidSize
-			);
+    public function removeGuild() {
+        $guildId = $_POST['form'][0]['value'];
 
-		die;
-	}
+        $sqlString = sprintf(
+            "DELETE 
+               FROM %s
+              WHERE guild_id = '%s'",
+            DbFactory::TABLE_GUILDS,
+            $guildId
+            );
+        die;
+    }
 
-	public function addNewGuild() {
-		$guild 		= $_POST['form'][0]['value'];
-		$server		= $_POST['form'][1]['value'];
-		$country 	= $_POST['form'][2]['value'];
+    public function editGuildHtml($guildDetails) {
+        $html = '';
+        $html .= '<form class="admin-form guild edit details" id="form-guild-edit-details" method="POST" action="http://localhost/stage/administrator">';
+        $html .= '<table class="admin-guild-listing">';
+        $html .= '<thead>';
+        $html .= '</thead>';
+        $html .= '<tbody>';
+        $html .= '<tr><th>Date Created</th></tr>';
+        $html .= '<tr><td>' . $guildDetails->_dateCreated . '</td></tr>';
+        $html .= '<tr><th>Leader</th></tr>';
+        $html .= '<tr><td><input type="text" name="text-guild-leader" value="' . $guildDetails->_leader . '"/></td></tr>';
+        $html .= '<tr><th>Website</th></tr>';
+        $html .= '<tr><td><input type="text" name="text-guild-website" value="' . $guildDetails->_website . '"/></td></tr>';
+        $html .= '<tr><th>Facebook</th></tr>';
+        $html .= '<tr><td><input type="text" name="text-guild-facebook" value="' . $guildDetails->_facebook . '"/></td></tr>';
+        $html .= '<tr><th>Twitter</th></tr>';
+        $html .= '<tr><td><input type="text" name="text-guild-twitter" value="' . $guildDetails->_twitter . '"/></td></tr>';
+        $html .= '<tr><th>Google</th></tr>';
+        $html .= '<tr><td><input type="text" name="text-guild-google" value="' . $guildDetails->_google . '"/></td></tr>';
+        $html .= '<tr><th>Faction</th></tr>';
+        $html .= '<tr><td><input type="text" name="text-guild-faction" value="' . $guildDetails->_faction . '"/></td></tr>';
+        $html .= '<tr><th>Server</th></tr>';
+        $html .= '<tr><td><input type="text" name="text-guild-server" value="' . $guildDetails->_server . '"/></td></tr>';
+        $html .= '<tr><th>Active</th></tr>';
+        $html .= '<tr><td><input type="text" name="text-guild-active" value="' . $guildDetails->_active . '"/></td></tr>';
+        $html .= '</tbody>';
+        $html .= '</table>';
+        $html .= '<input id="admin-submit-guild-edit" type="submit" value="Submit" />';
+        $html .= '</form>';
 
-		$sqlString = sprintf(
-			"INSERT INTO %s
-			(name, server, country)
-			values('%s', '%s', '%s')",
-			DbFactory::TABLE_GUILDS,
-			$guild,
-			$server,
-			$country
-			);
+        return $html;
+    }
 
-		die;
-	}
+    public function editGuild() {
+        $html         = '';
+        $guildId      = $_POST['form'][0]['value'];
+        $guildDetails = CommonDataContainer::$guildArray[$guildId];
+
+        //print_r($guildDetails);
+        //echo '<table class="listing"><thead><tr><th>test</th></thead></table>';
+        $html = $this->editGuildHtml($guildDetails);
+        
+        echo $html;
+        die;
+    }
+
+    public function editGuildDetails() {
+
+    }
 }
 
 ?>
--- public/js/administrator/administrator.js
+++ public/js/administrator/administrator.js
@@ -1,34 +1,33 @@
 var Administrator = function() {
-    var btnPressed;
-    var formName;
-
+    var id;
+    var activeDiv;
     // Submit admin forms
     $(document).on('submit', '.admin-form', function() {
         event.preventDefault();
 
-        var formData    = $(this).serializeArray();
-        var id          = $(this).prop('id').replace('form-', '');
-        var formName    = id.replace('-add', '').ucFirst();
+        var formData = $(this).serializeArray();
+        id           = $(this).prop('id').replace('form-', '');
+        //var formName    = id.replace('-', ' ').ucFirst();
 
+        console.log(id);
         $.ajax({
-            type:       'POST',
-            url:        'http://localhost/stage/administrator',
-            data:       {form:formData, request:id},
-            encode:     true,
-            success:    function() {
-                $(".overlay").fadeToggle('fast');
-                var activeDiv = $('#popup-wrapper');
-
-                activeDiv.toggleClass('centered');
-                activeDiv.fadeToggle('fast');
-                activeDiv.html(formName +  ' Added Successfully!');
+            type:    'POST',
+            url:     'http://localhost/stage/administrator',
+            data:    {form:formData, request:id},
+            encode:  true,
+            success: function(data) {
+                console.log(data);
+                    activeDiv = $('#admin-guild-listing');
+                    activeDiv.html(data);
             }, 
-            error:      function(data) {
+            error:  function(data) {
                 console.log(data);
             }
-        }).done(function(data) {
-            console.log(data);
         });
+    });
+
+    $(document).on('change', '.admin-select.guild.edit', function() {
+        var guildId = this.value;
     });
 
     // function to capitalize first letter of the form name
