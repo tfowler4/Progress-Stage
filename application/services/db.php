35150a7eac649f209c06f53a60395e51a8f39b1b
Terrijon Fowler <tfowler4@student.gsu.edu>
2015-08-18 02:06:51 -0400

    Database Singleton Practice implemented

--- application/services/db.php
+++ application/services/db.php
@@ -2,19 +2,30 @@
 class Db {
     protected static $_dbh;
 
-    public static function init() {
+    private function __construct() {
         try {
             self::$_dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=UTF8", DB_USER, DB_PASS);
             self::$_dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
         } catch ( PDOException $e ) {
             die('Connection error: ' . $e->getMessage());
         }
     }
+
+    public static function init() {
+        if ( !self::$_dbh ) {
+            new Db();
+        }
+    }
     
     public static function getDbh() {
+        if ( !self::$_dbh ) {
+            new Db();
+        }
+
         return self::$_dbh;
     }
+
+    public function __clone() {}
 }
 
 Db::init();
--- application/services/dbFactory.php
+++ application/services/dbFactory.php
@@ -1,5 +1,7 @@
 <?php
-class DbFactory extends Db {
+class DbFactory {
+    private static $_dbh;
+
     const TABLE_DUNGEONS     = 'dungeon_table';
     const TABLE_ENCOUNTERS   = 'encounterlist_table';
     const TABLE_FACTIONS     = 'faction_table';
@@ -17,34 +19,58 @@ class DbFactory extends Db {
     const TABLE_TWITCH       = 'twitch_table';
 
     public static function init() {
-        $query = self::getTwitchChannels();
-        while ($row = $query->fetch(PDO::FETCH_ASSOC)) { CommonDataContainer::$twitchArray[$row['twitch_num']] = $row; }
+        self::$_dbh = self::getDbh();
+        self::_getTwitchChannels();
+        self::_getCountries();
+        self::_getServers();
+        self::_getRegions();
+        self::_getFactions();
+        self::_getEncounters();
+        self::_getDungeons();
+        self::_getTiers();
+        self::_getRaidSizes();
+        self::_getTierSizes();
+        self::_getRankSystems();
+        self::_getGuilds();
+    }
 
-        $query = self::getCountries();
-        while ($row = $query->fetch(PDO::FETCH_ASSOC)) { CommonDataContainer::$countryArray[$row['name']] = new Country($row); }
-
-        $query = self::getServers();
+    private static function _getServers() {
+        $query = self::$_dbh->query(sprintf(
+            "SELECT *
+               FROM %s
+           ORDER BY region ASC, name ASC",
+            self::TABLE_SERVERS
+            ));
         while ($row = $query->fetch(PDO::FETCH_ASSOC)) { $row['name'] = utf8_encode($row['name']); CommonDataContainer::$serverArray[$row['name']] = new Server($row); }
+    }
 
-        $query = self::getRegions();
+    private static function _getRegions() {
+        $query = self::$_dbh->query(sprintf(
+            "SELECT *
+               FROM %s
+           ORDER BY abbreviation DESC",
+             self::TABLE_REGIONS
+             ));
         while ($row = $query->fetch(PDO::FETCH_ASSOC)) { CommonDataContainer::$regionArray[$row['abbreviation']] = new Region($row); }
-
-        $query = self::getFactions();
-        while ($row = $query->fetch(PDO::FETCH_ASSOC)) { CommonDataContainer::$factionArray[$row['name']] = new Faction($row); }
-        
-        $query = self::getEncounters();
-        while ($row = $query->fetch(PDO::FETCH_ASSOC)) { CommonDataContainer::$encounterArray[$row['encounter_id']] = new Encounter($row); }
-        
-        $query = self::getDungeons();
-        while ($row = $query->fetch(PDO::FETCH_ASSOC)) { CommonDataContainer::$dungeonArray[$row['dungeon_id']] = new Dungeon($row); }
-
-        $query = self::getTiers();
+    }
+    
+    private static function _getTiers() {
+        $query = self::$_dbh->query(sprintf(
+            "SELECT *
+               FROM %s
+           ORDER BY tier DESC",
+            self::TABLE_TIERS
+            ));
         while ($row = $query->fetch(PDO::FETCH_ASSOC)) { CommonDataContainer::$tierArray[$row['tier']] = new Tier($row); }
+    }
 
-        // CREATE RAID SIZE
-        foreach ( unserialize (RAID_SIZES) as $raidSize ) { CommonDataContainer::$raidSizeArray[$raidSize] = new RaidSize($raidSize); }
+    private static function _getRaidSizes() {
+        foreach ( unserialize (RAID_SIZES) as $raidSize ) {
+            CommonDataContainer::$raidSizeArray[$raidSize] = new RaidSize($raidSize);
+        }
+    }
 
-        // CREATE TIER SIZE
+    private static function _getTierSizes() {
         foreach ( CommonDataContainer::$tierArray as $tierId => $tierDetails ) {
             foreach ( CommonDataContainer::$raidSizeArray as $raidSize => $raidSizeDetails ) {
                 $tierSize = $tierDetails->_tier . '_' . $raidSizeDetails->_raidSize;
@@ -52,125 +78,79 @@ class DbFactory extends Db {
                 CommonDataContainer::$tierSizeArray[$tierSize] = new TierSize($tierDetails, $raidSize, $tierSize);
             } 
         }
-
-        $query = self::getRankSystems();
-        while ($row = $query->fetch(PDO::FETCH_ASSOC)) { CommonDataContainer::$rankSystemArray[$row['abbreviation']] = new RankSystem($row); }  
-
-        $query = self::getGuilds();
-        while ( $row = $query->fetch(PDO::FETCH_ASSOC) ) { CommonDataContainer::$guildArray[$row['guild_id']] = new GuildDetails($row); }
     }
 
-    public static function getServers() {
-        $query = self::$_dbh->query(sprintf(
-            "SELECT *
-               FROM %s
-           ORDER BY region ASC, name ASC",
-            self::TABLE_SERVERS
-            ));
-        return $query;
-    }
-    
-    public static function getRegions() {
-        $query = self::$_dbh->query(sprintf(
-            "SELECT *
-               FROM %s
-           ORDER BY abbreviation DESC",
-             self::TABLE_REGIONS
-             ));
-        return $query;
-    }
-    
-    public static function getTiers() {
-        $query = self::$_dbh->query(sprintf(
-            "SELECT *
-               FROM %s
-           ORDER BY tier DESC",
-            self::TABLE_TIERS
-            ));
-        return $query;
-    }
-    
-    public static function getDungeons() {
+    private static function _getDungeons() {
         $query = self::$_dbh->query(sprintf(
             "SELECT *
                FROM %s
            ORDER BY players DESC, dungeon_id DESC",
             self::TABLE_DUNGEONS
             ));
-        return $query;
+        while ($row = $query->fetch(PDO::FETCH_ASSOC)) { CommonDataContainer::$dungeonArray[$row['dungeon_id']] = new Dungeon($row); }
     }
     
-    public static function getEncounters() {
+    private static function _getEncounters() {
         $query = self::$_dbh->query(sprintf(
             "SELECT *
                FROM %s
            ORDER BY tier DESC, dungeon DESC, mob_order ASC",
             self::TABLE_ENCOUNTERS
             ));
-        return $query;
+        while ($row = $query->fetch(PDO::FETCH_ASSOC)) { CommonDataContainer::$encounterArray[$row['encounter_id']] = new Encounter($row); }
     }
-    
-    public static function getCountries() {
+
+    private static function _getCountries() {
         $query = self::$_dbh->query(sprintf(
             "SELECT *
                FROM %s
            ORDER BY name ASC",
             self::TABLE_COUNTRIES
             ));
-        return $query;
+        while ($row = $query->fetch(PDO::FETCH_ASSOC)) { CommonDataContainer::$countryArray[$row['name']] = new Country($row); }
     }
     
-    public static function getFactions() {
+    private static function _getFactions() {
         $query = self::$_dbh->query(sprintf(
             "SELECT *
                FROM %s
            ORDER BY faction_id DESC",
             self::TABLE_FACTIONS
             ));
-        return $query;
+        while ($row = $query->fetch(PDO::FETCH_ASSOC)) { CommonDataContainer::$factionArray[$row['name']] = new Faction($row); }
     }
-    
-    public static function getGuilds() {
+
+    private static function _getGuilds() {
         $query = self::$_dbh->query(sprintf(
             "SELECT *
                FROM %s
            ORDER BY name ASC",
             self::TABLE_GUILDS
             ));
-        return $query;
-    }
-    
-    public static function getEmails() {
-        $query = self::$_dbh->query(sprintf(
-            "SELECT username, email
-               FROM %s
-           ORDER BY email ASC",
-            self::TABLE_USERS
-            ));
-        return $query;
+        while ( $row = $query->fetch(PDO::FETCH_ASSOC) ) { CommonDataContainer::$guildArray[$row['guild_id']] = new GuildDetails($row); }
     }
 
-    public static function getRankSystems() {
+    private static function _getRankSystems() {
         $query = self::$_dbh->query(sprintf(
             "SELECT *
                FROM %s
            ORDER BY system_id ASC",
             self::TABLE_SYSTEMS
             ));
-        return $query;
+        while ($row = $query->fetch(PDO::FETCH_ASSOC)) { CommonDataContainer::$rankSystemArray[$row['abbreviation']] = new RankSystem($row); }
     }
 
-    public static function getTwitchChannels() {
+    private static function _getTwitchChannels() {
         $query = self::$_dbh->query(sprintf(
             "SELECT *
                FROM %s",
             self::TABLE_TWITCH
             ));
-        return $query;
+        while ($row = $query->fetch(PDO::FETCH_ASSOC)) { CommonDataContainer::$twitchArray[$row['twitch_num']] = $row; }
     }
 
     public static function getDbh() {
-        return self::$_dbh;
+        return Db::getDbh();
     }
 }
 
