1fc0f285c9f9085d1ac4837c1ce45eeb611b7b58
Terrijon Fowler <tfowler4@student.gsu.edu>
2015-11-20 18:34:34 -0500

    Rankings fix for guild signatures

--- application/models/GuildModel/class/GuildSignature.php
+++ application/models/GuildModel/class/GuildSignature.php
@@ -12,6 +12,7 @@ class GuildSignature {
     protected $_sigDetails;
     protected $_guildDetails;
     protected $_dungeonDetails;
+    protected $_rankDetails;
 
     /**
      * constructor
@@ -26,7 +27,8 @@ class GuildSignature {
         if ( isset($params[1]) ) { $this->_dungeonId  = $params[1]; } else { die; }
         if ( isset($params[2]) ) { $this->_rankSystem = $params[2]; } else { die; }
         if ( isset($params[3]) ) { $this->_view       = $params[3]; } else { die; }
-        $this->getDetails();
+
+        $this->_getDetails();
     }
 
     /**
@@ -34,15 +36,15 @@ class GuildSignature {
      * 
      * @return void
      */
-    public function getDetails() {
+    protected function _getDetails() {
         if ( !isset(CommonDataContainer::$guildArray[$this->_guildId]) ) { die; }
 
         $this->_guildDetails = CommonDataContainer::$guildArray[$this->_guildId];
-        $this->_getAllGuildDetails();
+        $this->_rankDetails  = $this->_getRankingsForGuild($this->_dungeonId, $this->_rankSystem, $this->_view, $this->_guildId);
 
         $this->_dungeonDetails = CommonDataContainer::$dungeonArray[$this->_dungeonId];
 
-        $this->getHTML($this->_guildDetails);
+        $this->_getHTML($this->_guildDetails);
     }
 
     /**
@@ -52,7 +54,7 @@ class GuildSignature {
      * 
      * @return void
      */
-    public function getHTML($guildDetails) {
+    protected function _getHTML($guildDetails) {
         $boxName    = array();
         $boxRank    = array();
         $boxPoints  = array();
@@ -100,13 +102,9 @@ class GuildSignature {
         $standingText       = '0/' . $this->_dungeonDetails->_numOfEncounters . ' ' . $this->_dungeonDetails->_abbreviation;
         $rankSystemText     = CommonDataContainer::$rankSystemArray[$this->_rankSystem]->_name;
 
-        if ( $this->_guildDetails->_dungeonDetails->{$this->_dungeonId}->_complete > 0 ) {
-            $standingText = $this->_guildDetails->_dungeonDetails->{$this->_dungeonId}->_standing;
-        }
-
-        if ( isset($this->_guildDetails->_dungeonDetails->{$this->_dungeonId}->{'_' . strtolower($this->_rankSystem)}) ) {
-            $rankDetails = $this->_guildDetails->_dungeonDetails->{$this->_dungeonId}->{'_' . strtolower($this->_rankSystem)};
-            $rankText    = $this->_viewText . ' ' . Functions::convertToOrdinal($rankDetails->_rank->{$this->_view});
+        if ( !empty($this->_rankDetails) ) {
+            $standingText = $this->_rankDetails->_progress;
+            $rankText = $this->_viewText . ' ' . Functions::convertToOrdinal($this->_rankDetails->_rank);
         }
 
         // Add Flag and Region/Server text
@@ -132,53 +130,99 @@ class GuildSignature {
     }
 
     /**
-     * generate all encounter standings and rankings information
-     *      * 
+     * get a list of all standing data for a specific dungeon
+     *
+     * @param  string  $dungeonId [ id for dungeon ]
+     * @param  string  $limit     [ limit number of guilds to get ]
+     * @param  string  $view      [ viewing type of world/region/country/server ]
+     * @param  integer $guildId   [ id of guild ]
+     * 
      * @return void
      */
-    public function _getAllGuildDetails() {
-        $dbh       = DbFactory::getDbh();
-        $dataArray = array();
+    protected function _getRankingsForGuild($dungeonId, $rankType, $view, $guildId) {
+        $dbh = DbFactory::getDbh();
 
-        $query = $dbh->prepare(sprintf(
-            "SELECT kill_id,
-                    guild_id,
-                    encounter_id,
-                    dungeon_id,
-                    tier,
-                    raid_size,
-                    datetime,
-                    date,
-                    time,
-                    time_zone,
-                    server,
-                    videos,
-                    server_rank,
-                    region_rank,
-                    world_rank,
-                    country_rank
-               FROM %s
-              WHERE guild_id=%d", 
-                    DbFactory::TABLE_KILLS, 
-                    $this->_guildDetails->_guildId
-                ));
-        $query->execute();
+        $query = $dbh->query(sprintf(
+                    "SELECT %s.guild_id,
+                            %s.dungeon_id,
+                            %s.recent_time,
+                            %s.recent_activity,
+                            %s.world_first,
+                            %s.region_first,
+                            %s.server_first,
+                            %s.country_first,
+                            %s.progress,
+                            %s.complete,
+                            %s.special_progress,
+                            qp_points,
+                            qp_world_rank,
+                            qp_region_rank,
+                            qp_server_rank,
+                            qp_country_rank,
+                            qp_world_trend,
+                            qp_region_trend,
+                            qp_server_trend,
+                            qp_country_trend,
+                            qp_world_prev_rank,
+                            qp_region_prev_rank,
+                            qp_server_prev_rank,
+                            qp_country_prev_rank,
+                            ap_points,
+                            ap_world_rank,
+                            ap_region_rank,
+                            ap_server_rank,
+                            ap_country_rank,
+                            ap_world_trend,
+                            ap_region_trend,
+                            ap_server_trend,
+                            ap_country_trend,
+                            ap_world_prev_rank,
+                            ap_region_prev_rank,
+                            ap_server_prev_rank,
+                            ap_country_prev_rank,
+                            apf_points,
+                            apf_world_rank,
+                            apf_region_rank,
+                            apf_server_rank,
+                            apf_country_rank,
+                            apf_world_trend,
+                            apf_region_trend,
+                            apf_server_trend,
+                            apf_country_trend,
+                            apf_world_prev_rank,
+                            apf_region_prev_rank,
+                            apf_server_prev_rank,
+                            apf_country_prev_rank
+                       FROM %s
+            LEFT OUTER JOIN %s
+                         ON %s.guild_id = %s.guild_id
+                         WHERE %s.dungeon_id = %d
+                           AND %s.dungeon_id = %d",
+            DbFactory::TABLE_RANKINGS,
+            DbFactory::TABLE_RANKINGS,
+            DbFactory::TABLE_STANDINGS,
+            DbFactory::TABLE_STANDINGS,
+            DbFactory::TABLE_STANDINGS,
+            DbFactory::TABLE_STANDINGS,
+            DbFactory::TABLE_STANDINGS,
+            DbFactory::TABLE_STANDINGS,
+            DbFactory::TABLE_STANDINGS,
+            DbFactory::TABLE_STANDINGS,
+            DbFactory::TABLE_STANDINGS,
+            DbFactory::TABLE_RANKINGS,
+            DbFactory::TABLE_STANDINGS,
+            DbFactory::TABLE_RANKINGS,
+            DbFactory::TABLE_STANDINGS,
+            DbFactory::TABLE_RANKINGS,
+            $dungeonId,
+            DbFactory::TABLE_STANDINGS,
+            $dungeonId,
+            DbFactory::TABLE_STANDINGS,
+            $guildId
+        ));
 
         while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-            $encounterId         = $row['encounter_id'];
-            $encounterDetails    = CommonDataContainer::$encounterArray[$encounterId];
-            $dungeonId           = $encounterDetails->_dungeonId;
-            $dungeonDetails      = CommonDataContainer::$dungeonArray[$dungeonId];
-            $tierId              = $dungeonDetails->_tier;
-            $tierDetails         = CommonDataContainer::$tierArray[$tierId];
-
-            $arr = $this->_guildDetails->_progression;
-            $arr['dungeon'][$dungeonId][$encounterId] = $row;
-            $arr['encounter'][$encounterId] = $row;
-            $this->_guildDetails->_progression = $arr;
+            return new RankingsDataObject($row, $rankType, $view);
         }
-
-        $this->_guildDetails->generateEncounterDetails('dungeon', $this->_dungeonId);
-        $this->_guildDetails->generateRankDetails('dungeons');
     }
 }
