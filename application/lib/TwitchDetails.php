70ffd80dccc05f48cd644f40748ba2c5f8b615d9
Terrijon Fowler <tfowler4@student.gsu.edu>
2016-03-27 19:18:53 -0400

    Fixed media slider guild logo sizes

--- application/lib/TwitchDetails.php
+++ application/lib/TwitchDetails.php
@@ -9,6 +9,7 @@ class TwitchDetails extends DetailObject {
     protected $_active;
     protected $_hyperlink;
     protected $_guildDetails;
+    protected $_guildLogo;
     protected $_twitchId;
 
     /**
@@ -19,6 +20,25 @@ class TwitchDetails extends DetailObject {
         $this->_url          = $params['twitch_url'];
         $this->_active       = $params['active'];
         $this->_guildDetails = CommonDataContainer::$guildArray[$params['guild_id']];
+
+        $imgSrc              = FOLD_GUILD_LOGOS . 'logo-' . $this->_guildDetails->_guildId;
+        $localSrc            = ABS_FOLD_SITE_GUILD_LOGOS . 'logo-' .  $this->_guildDetails->_guildId;
+        $class = '';
+
+        if ( file_exists($localSrc) && getimagesize($localSrc) ) {
+            $imageDimensions = getimagesize($localSrc);
+
+            $width  = $imageDimensions[0];
+            $height = $imageDimensions[1];
+
+            if ( $width > 200 ) {
+                $class = 'class="img-responsive" style="margin-top:5px; max-height:75px;"';
+            } else {
+                $class = 'style="margin-top: ' . (($height/4)*-1 - 10) . 'px;"';
+            }
+
+            $this->_guildLogo    = '<img src="' . $imgSrc . '" ' .  $class . ' >';
+        }
 
         $path = ABS_FOLD_TWITCH . $params['twitch_id'];
         $this->_image = '<img src="' . FOLD_TWITCH . $params['twitch_id'] . '">';
